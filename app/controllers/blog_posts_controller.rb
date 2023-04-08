2b454b4246f4bb7f6539811177c41d5ab2157bdb
philippe2023 <philippe.webdeveloper@gmail.com>
2023-04-08 22:40:21 +0200

    setup show and links to display posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,17 @@ class BlogPostsController < ApplicationController
     def index
         @blog_posts = BlogPost.all
     end
+
+    def show
+        @blog_post = BlogPost.find(params[:id])
+            # the following line serves to redirect to the root folder
+            # in case the requested id do not exist to that we do not have an error message.
+            rescue ActiveRecord::RecordNotFound
+                redirect_to root_path
+    end
+
+    private
+    def method_name
+        
+    end
 end
