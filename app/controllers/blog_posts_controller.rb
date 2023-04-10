db9b9ab16a1db0be4334932fb8bf50965e4ea21a
philippe2023 <philippe.webdeveloper@gmail.com>
2023-04-10 14:10:24 +0200

    CRUD completed

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,14 +1,17 @@
 class BlogPostsController < ApplicationController
+    # before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+    before_action :set_blog_post, except: [:index, :new, :create]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
+        # @blog_post = BlogPost.find(params[:id])
             # the following line serves to redirect to the root folder
             # in case the requested id do not exist to that we do not have an error message.
-            rescue ActiveRecord::RecordNotFound
-                redirect_to root_path
+            # rescue ActiveRecord::RecordNotFound
+            #     redirect_to root_path
     end
 
     def new
@@ -25,11 +28,11 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
+        # @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
+        # @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
@@ -37,8 +40,20 @@ class BlogPostsController < ApplicationController
         end
     end
 
+    def destroy
+        # @blog_post = BlogPost.find(params[:id])
+        @blog_post.destroy
+        redirect_to root_path
+    end
+
     private
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  # get "/blog_posts", to: "blog_posts#index", as: :blog_post
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   root "blog_posts#index"
+  resources :blog_posts
 end
