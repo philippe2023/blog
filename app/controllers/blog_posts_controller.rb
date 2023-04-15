18c5bd4bd9b0ba9de01f9c631c5249612c51e13d
philippe2023 <philippe.webdeveloper@gmail.com>
2023-04-15 16:59:22 +0200

    added draft, published at  and scheduled function as well as sorted by time frame

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,7 @@ class BlogPostsController < ApplicationController
     before_action :set_blog_post, except: [:index, :new, :create]
 
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show
@@ -49,11 +49,15 @@ class BlogPostsController < ApplicationController
 
     private
     def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
     def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
+        if user_signed_in?
+            @blog_post = BlogPost.find(params[:id])            
+        else
+            @blog_post = BlogPost.published.find(params[:id])            
+        end
         rescue ActiveRecord::RecordNotFound
             redirect_to root_path
     end
